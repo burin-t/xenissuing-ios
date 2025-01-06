e18b241719b60f5f937367d9ba72b99fabf30221
burin-t <100820279+burin-t@users.noreply.github.com>
2025-01-06 15:59:15 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     // Set minimum deployment target to macOS 10.15 and iOS 14.
     platforms: [
         .macOS(.v10_15),
-        .iOS(.v14)
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
